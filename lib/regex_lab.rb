e6ac5b7247d13a0d8238382e88ac0ab61eba0674
shadowfox3735 <normanpan180@gmail.com>
2018-11-01 21:35:01 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-  vowel = word.scan(/\b[aeiou]/)
+  vowel = word.scan(/\b[aeiou|AEIOU]/)
   if vowel != []
     TRUE
   else
@@ -16,7 +16,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match()
 end
 
 def valid_phone_number?(phone)
