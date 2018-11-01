7d203d4ae609a02db19fe327186f064394564cef
shadowfox3735 <normanpan180@gmail.com>
2018-11-01 21:55:08 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,32 @@
 def starts_with_a_vowel?(word)
-
+  vowel = word.scan(/\b[aeiou|AEIOU]/)
+  if vowel != []
+    TRUE
+  else
+    FALSE
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/\A[AEIOU]/) && text.match(/\W$/)
+    TRUE
+  else
+    FALSE
+  end
 end
 
 def valid_phone_number?(phone)
-
+ if phone.scan(/\d/).length == 10
+   TRUE
+ else
+   FALSE
+ end
 end
