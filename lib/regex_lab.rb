0e8a2feab19440a875bfba66cb8ac11610564c5e
shadowfox3735 <normanpan180@gmail.com>
2018-11-01 21:54:54 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -24,5 +24,9 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
- iphone.grep(/\d/).length
+ if phone.scan(/\d/).length == 10
+   TRUE
+ else
+   FALSE
+ end
 end
